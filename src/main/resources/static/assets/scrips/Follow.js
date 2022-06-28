8a846d34ac2e9be97eb3c67fac2f9a649fb8918c
Your Name <daurei123@gmail.com>
2022-06-28 21:24:50 +0700

    test

--- src/main/resources/static/assets/scrips/Follow.js
+++ src/main/resources/static/assets/scrips/Follow.js
@@ -7,8 +7,8 @@ function getFollow() {
         const follows = $('.follows ul')
         for (const follow of result) {
             res = `
-                <li><a href="http://localhost:8081/profile/${follow.followedId}" style="display: flex;align-items: center;">
-                    <img src="http://localhost:8081/${follow.followAvatar}" alt="" class="avatar" width="36px" height="36px" />
+                <li><a href="https://hungdeptraino1.herokuapp.com//profile/${follow.followedId}" style="display: flex;align-items: center;">
+                    <img src="https://hungdeptraino1.herokuapp.com//${follow.followAvatar}" alt="" class="avatar" width="36px" height="36px" />
                     <strong style="font-size: 20px;padding-left: 8px;">${follow.followName}</strong>
                 </a></li>
             `
